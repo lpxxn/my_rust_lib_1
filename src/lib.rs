3433ca1099342c924e6e49970e11ec9037d25144
lpxxn <mi_duo@live.com>
2020-08-29 18:43:33 +0800

    test lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub mod user;
+pub use user::User;
+
 pub fn add(x: i32, y: i32) -> i32 {
     x + y
 }
@@ -7,6 +10,11 @@ mod tests {
     #[test]
     fn add() {
         assert_eq!(crate::add(2, 2), 4);
+    }
+    #[test]
+    fn user_name() {
+        let u = crate::User::new_user(String::from("li"), 10);
+        assert_eq!(u.name(), "li")
     }
 }
 
